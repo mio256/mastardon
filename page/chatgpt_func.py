f7363aa42e8560ccf8fa6667531d4b267fc58443
mio256 <reiya@woorth.io>
2023-05-12 23:35:59 +0900

    :zap: chatgpt_func

--- page/chatgpt_func.py
+++ page/chatgpt_func.py
@@ -7,37 +7,25 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 
 def analyze_toot(mastodon_timelines: list):
-    messages = [
-        {
-            "role": "system",
-            "content": """
-                Analyze the tweets and arrange them in order of relevance to programming.
-                The format for responses is below.
-                DON'T send response outside of the format, as it crash the program.
-                ```
-                {id}, {id}, {id}, {id}, {id}
-                ```
-                """
-        },
-    ]
-    for i in mastodon_timelines:
-        messages.append({
-            "role": "user",
-            "content": f"id={i.id}\n {i.content}"
-        })
     r = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=messages
-    )["choices"][0]["message"]["content"].split(',')
-    # print(res)
+        messages=[
+            {
+                "role": "user",
+                "content": ''.join([f"id={i.id} - {re.sub(re.compile('<.*?>'), '', i.content)}\n" for i in mastodon_timelines]),
+            },
+            {
+                "role": "system",
+                "content": "Pull out sentence about programming from these. You need use this format for response. `{id}, {id}, {id}, {id}, {id}`",
+            },
+        ]
+    )["choices"][0]["message"]["content"]
 
     l = []
-    for x in r:
-        x = re.sub(r"\D", "", x)
+    for x in r.split(','):
         try:
-            l.append(int(x))
+            l.append(int(re.sub(r"\D", "", x)))
         except ValueError:
             pass
-    # print(l)
 
-    return l
+    return l, r
