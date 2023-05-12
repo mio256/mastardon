dda57637ce84bc278820aa886bf02ccce0e5a467
mio256 <reiya@woorth.io>
2023-05-12 13:02:45 +0900

    :fire: IndexView.context['timeline']

--- page/views.py
+++ page/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+import os
 from django.http import JsonResponse
 from django.views.generic import TemplateView
+from mastodon import Mastodon
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def PingView(request):
@@ -9,3 +12,15 @@ def PingView(request):
 
 class IndexView(TemplateView):
     template_name = "page/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        mastodon = Mastodon(
+            os.environ['CLIENT_ID'],
+            os.environ['CLIENT_SECRET'],
+            os.environ['ACCESS_TOKEN'],
+            os.environ['API_BASE_URL'],
+        )
+        response = mastodon.timeline_home()
+        context['timeline'] = response
+        return context
