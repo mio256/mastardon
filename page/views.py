0884485d9f2756a52b535e5219470aada5bdd0fa
mio256 <reiya@woorth.io>
2023-05-13 00:02:37 +0900

    :fire: delete en mode

--- page/views.py
+++ page/views.py
@@ -17,14 +17,10 @@ class IndexView(TemplateView):
 
         page = int(self.request.GET.get('page', 3))
         mode = self.request.GET.get('mode', None)
-        reb = int(self.request.GET.get('reb', 2))
-        fav = int(self.request.GET.get('fav', 1))
 
         mastodon_timelines = mastodon_func.timelines(page)
-
         if mode == 'gpt':
             id_list, gpt_response = chatgpt_func.analyze_toot(mastodon_timelines)
-
             timeline = []
             for i in range(len(mastodon_timelines)):
                 for id in id_list:
@@ -32,9 +28,9 @@ class IndexView(TemplateView):
                         timeline.append(mastodon_timelines[i])
             context['gpt_response'] = gpt_response
             context['timeline'] = timeline
-        elif mode == 'en':
-            context['timeline'] = sorted(mastodon_timelines, key=lambda x: -(x.reblogs_count * reb + x.favourites_count * fav + len(re.findall('[a-z]+', x.content))))
         elif mode == 'sort':
+            reb = int(self.request.GET.get('reb', 2))
+            fav = int(self.request.GET.get('fav', 1))
             context['timeline'] = sorted(mastodon_timelines, key=lambda x: -(x.reblogs_count * reb + x.favourites_count * fav))
         else:
             context['timeline'] = mastodon_timelines
