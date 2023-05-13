82374bc204d281b35d3868660f06d33db9b127d3
mio256 <reiya@woorth.io>
2023-05-13 16:22:07 +0900

    :sparkles: timelines_hours

--- page/mastodon_func.py
+++ page/mastodon_func.py
@@ -1,20 +1,34 @@
 import os
+import datetime
 from mastodon import Mastodon
 from dotenv import load_dotenv
 load_dotenv()
 
+mastodon = Mastodon(
+    os.environ['CLIENT_ID'],
+    os.environ['CLIENT_SECRET'],
+    os.environ['ACCESS_TOKEN'],
+    os.environ['API_BASE_URL'],
+)
 
-def timelines(page: int = 5):
-    mastodon = Mastodon(
-        os.environ['CLIENT_ID'],
-        os.environ['CLIENT_SECRET'],
-        os.environ['ACCESS_TOKEN'],
-        os.environ['API_BASE_URL'],
-    )
 
+def timelines_page(page: int):
     responses = []
     r = mastodon.timeline_home()
     for _ in range(page):
         responses += r
         r = mastodon.timeline_home(max_id=r[-1].id)
+    return responses
+
+
+def timelines_hours(hours: int):
+    d = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=hours)
+    responses = []
+
+    r = mastodon.timeline_home()
+    responses += r
+    while r[-1].created_at > d:
+        r = mastodon.timeline_home(max_id=r[-1].id)
+        responses += r
+
     return responses
--- page/views.py
+++ page/views.py
@@ -1,5 +1,3 @@
-import re
-import pprint
 from . import mastodon_func, chatgpt_func
 from django.http import JsonResponse
 from django.views.generic import TemplateView
@@ -15,11 +13,22 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        page = int(self.request.GET.get('page', 3))
-        mode = self.request.GET.get('mode', None)
+        if self.request.GET.get('hours'):
+            hours = int(self.request.GET.get('hours'))
+            mastodon_timelines = mastodon_func.timelines_hours(hours)
+        else:
+            page = int(self.request.GET.get('page', 3))
+            mastodon_timelines = mastodon_func.timelines_page(page)
 
-        mastodon_timelines = mastodon_func.timelines(page)
+        mode = self.request.GET.get('mode')
+
         if mode == 'gpt':
+            if self.request.GET.get('high'):
+                high = int(self.request.GET.get('high', 20))
+                reb = int(self.request.GET.get('reb', 2))
+                fav = int(self.request.GET.get('fav', 1))
+                mastodon_timelines = sorted(mastodon_timelines, key=lambda x: -(x.reblogs_count * reb + x.favourites_count * fav))
+                mastodon_timelines = mastodon_timelines[:high]
             id_list, gpt_response = chatgpt_func.analyze_toot(mastodon_timelines)
             timeline = []
             for i in range(len(mastodon_timelines)):
@@ -31,7 +40,11 @@ class IndexView(TemplateView):
         elif mode == 'sort':
             reb = int(self.request.GET.get('reb', 2))
             fav = int(self.request.GET.get('fav', 1))
-            context['timeline'] = sorted(mastodon_timelines, key=lambda x: -(x.reblogs_count * reb + x.favourites_count * fav))
+            mastodon_timelines = sorted(mastodon_timelines, key=lambda x: -(x.reblogs_count * reb + x.favourites_count * fav))
+            if self.request.GET.get('high'):
+                high = int(self.request.GET.get('high', 20))
+                mastodon_timelines = mastodon_timelines[:high]
+            context['timeline'] = mastodon_timelines
         else:
             context['timeline'] = mastodon_timelines
 
