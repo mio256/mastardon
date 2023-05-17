8058c594fb7197f7164299ac9716a181081ced20
mio256 <reiya@woorth.io>
2023-05-17 16:58:35 +0900

    :test_tube: for TDD / PingTestCase, IndexTestCase

--- page/tests.py
+++ page/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.http import JsonResponse
+from django.template.response import TemplateResponse
 
-# Create your tests here.
+
+class PingTestCase(TestCase):
+    def testResponseType(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(type(response), JsonResponse)
+
+    def testResponseContent(self):
+        response = self.client.get('/ping/')
+        self.assertEqual(response.json(), {'result': True})
+
+
+class IndexTestCase(TestCase):
+    def testResponseType(self):
+        response = self.client.get('/')
+        self.assertEqual(type(response), TemplateResponse)
+
+    def testResponseTypeSort(self):
+        response = self.client.get('/?mode=sort')
+        self.assertEqual(type(response), TemplateResponse)
+
+    def testResponseTypeGPT(self):
+        response = self.client.get('/?mode=gpt')
+        self.assertEqual(type(response), TemplateResponse)
