628eb8c8a593b3ccb7143726327802b5d2cc96c9
mio256 <reiya@woorth.io>
2023-05-18 16:56:41 +0900

    :test_tube: MastodonFuncTest, ChatGPTFuncTest

--- page/tests.py
+++ page/tests.py
@@ -1,19 +1,60 @@
+import datetime
+import random
+from mastodon import utility
 from django.test import TestCase
 from django.urls import reverse
+from . import chatgpt_func, mastodon_func
+
+RESPONSE_LENGTH = 20
+TIMELINE_PAGE = random.randrange(5) + 1
+TIMELINE_HOURS = random.randrange(5) + 1
 
 
 class PingViewTest(TestCase):
-    def test_ping_view(self):
+    def test_ping_view_response(self):
         response = self.client.get(reverse('ping_view'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'{"result": true}')
 
 
 class IndexViewTest(TestCase):
-    def test_index_view_no_query_params(self):
+    def test_index_view_response_without_query_params(self):
         response = self.client.get(reverse('index_view'))
         self.assertEqual(response.status_code, 200)
 
-    def test_index_view_with_query_params(self):
+    def test_index_view_response_with_query_params(self):
         response = self.client.get(reverse('index_view'), {'hours': '2', 'mode': 'sort', 'reb': '1', 'fav': '2', 'high': '50'})
         self.assertEqual(response.status_code, 200)
+
+
+class MastodonFuncTest(TestCase):
+    def setUp(self):
+        self.response = mastodon_func.fetch_timeline()
+
+    def test_fetch_timeline_response(self):
+        self.assertIsInstance(self.response, utility.AttribAccessList)
+        self.assertEqual(len(self.response), RESPONSE_LENGTH)
+
+    def test_timelines_page_response(self):
+        response = mastodon_func.timelines_page(TIMELINE_PAGE)
+        self.assertIsInstance(response, list)
+        self.assertEqual(len(response), RESPONSE_LENGTH)
+
+    def test_timeline_hours_response(self):
+        response = mastodon_func.timelines_hours(TIMELINE_HOURS)
+        self.assertIsInstance(response, list)
+        d = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=TIMELINE_HOURS)
+        self.assertLessEqual(response[-1].created_at, d)
+
+
+class ChatGPTFuncTest(TestCase):
+    def test_request_chatgpt_response(self):
+        response = chatgpt_func.request_chatgpt(messages=[{"role": "user", "content": "hello"},])
+        self.assertEqual(response["choices"][0]["message"]["role"], "assistant")
+        self.assertIsInstance(response["choices"][0]["message"]["content"], str)
+
+    def test_analyze_toots_response(self):
+        mastodon_timelines = mastodon_func.fetch_timeline()
+        id_list, gpt_response = chatgpt_func.analyze_toots(mastodon_timelines)
+        self.assertIsInstance(id_list, list)
+        self.assertIsInstance(gpt_response, str)
